b2c3530991792bb2f13bf37d4ace9dfbeff4b81a
Dmitrii Sorin <info@staypositive.ru>
2017-11-15 16:49:45 +1100

    typo fix

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,4 @@
-// Identity
-const id = x => y => x === y;
+const eq = x => y => x === y;
 const not = fn => x => !fn(x);
 
 const isXError = x => error => error.keyword === x;
@@ -9,7 +8,7 @@ export const isEnumError = isXError('enum');
 
 export const getChildren = node => (node && Object.values(node.children)) || [];
 export const getSiblings = parent => node =>
-  getChildren(parent).filter(not(id(node)));
+  getChildren(parent).filter(not(eq(node)));
 export const getErrors = node => (node && node.errors) || [];
 
 export const concatAll = xs => ys => ys.reduce((zs, z) => zs.concat(z), xs);
