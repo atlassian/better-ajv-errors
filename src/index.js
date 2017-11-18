6f676eff796abb984c1f635dffa6edca858ad041
Dmitrii Sorin <dsorin@atlassian.com>
2017-11-18 16:43:42 +1100

    Revert "Move customError* functions outside of returned function scope"
    This reverts commit ed36098166797ce1e71a7b051db217e33a13d3cb.

--- src/index.js
+++ src/index.js
@@ -2,19 +2,21 @@ import prettify from './helpers';
 import { log } from './debug';
 
 export default (options = { schema, mode, indent }) => {
-  const customErrorToText = (error, data) =>
-    error.print(schema, data).join('\n');
-  const customErrorToStructure = (error, data) => error.getError(schema, data);
-
-  const { schema, mode = 'print', indent = 2 } = options;
+  const {
+    schema,
+    mode = 'print',
+    indent = 2,
+  } = options;
 
   return (data, errors) => {
+    const customErrorToText = (error) => error.print(schema, data).join('\n');
+    const customErrorToStructure = (error) => error.getError(schema, data);
     const customErrors = prettify(errors, indent);
 
     if (mode === 'print') {
-      log(customErrors.map(error => customErrorToText(error, data)).join());
+      log(customErrors.map(customErrorToText).join());
     } else {
-      return customErrors.map(error => customErrorToStructure(error, data));
+      return customErrors.map(customErrorToStructure);
     }
   };
 };
