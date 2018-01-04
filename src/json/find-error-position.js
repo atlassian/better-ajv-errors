74cce85e05938855d3639a9e659b15184dad474c
Tong Li <tongli0809@gmail.com>
2018-01-04 14:37:04 +1100

    Use Symbol for meta and value to avoid property name collision

--- src/json/find-error-position.js
+++ src/json/find-error-position.js
@@ -1,13 +1,13 @@
-import parse from './parse';
+import parse, { META_SYMBOL, VALUE_SYMBOL } from './parse';
 
 export default function findErrorPosition(jsonString, dataPath) {
   // console.log(JSON.stringify(parse(jsonString), null, 2));
   // TODO: Handle json pointer escape notation and better error handling
-  const { meta: { line, column } } = dataPath
+  const { [META_SYMBOL]: { line, column } } = dataPath
     .split('/')
     .slice(1)
     .reduce(
-      (obj, pointer) => obj.value[pointer] || obj.value.value[pointer],
+      (obj, pointer) => obj[VALUE_SYMBOL][pointer] || obj[VALUE_SYMBOL][VALUE_SYMBOL][pointer],
       parse(jsonString)
     );
 
