7a1d4c51c7e7c2952cd7156d7ed2efac9b98ada2
Jerome Wang <jwang2@atlassian.com>
2018-10-09 10:21:18 +1100

    remove package-lock

--- src/json/__tests__/index.js
+++ src/json/__tests__/index.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs';
 import parse from 'json-to-ast';
 import { getFixturePath } from 'jest-fixtures';
-import { getMetaFromPath } from '../';
+import { getMetaFromPath, getDecoratedDataPath } from '../';
 
 async function loadScenario(n) {
   const fixturePath = await getFixturePath(__dirname, `scenario-${n}.json`);
@@ -40,6 +40,6 @@ describe('JSON', () => {
   it('can work on JSON with Array with empty children', async () => {
     const rawJson = await loadScenario(4);
     const jsonAst = parse(rawJson, { loc: true });
-    expect(getMetaFromPath(jsonAst, '/arr/3')).toMatchSnapshot();
+    expect(getDecoratedDataPath(jsonAst, '/arr/4')).toMatchSnapshot();
   });
 });
