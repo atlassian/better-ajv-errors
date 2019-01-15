b1e0801e63789be8f09616e148e0802243501ced
Mike Ralphson <mike.ralphson@gmail.com>
2019-01-15 16:58:26 +0000

    Copy new test cases from torifat/better-ajv-errors PR-1

--- src/json/__tests__/index.js
+++ src/json/__tests__/index.js
@@ -57,5 +57,21 @@ describe('JSON', () => {
     });
     const jsonAst = parse(rawJsonWithArrayItem, { loc: true });
     expect(getDecoratedDataPath(jsonAst, '/arr/3')).toMatchSnapshot();
+  })
+
+  it('can work with unescaped JSON pointers with ~1', async () => {
+    const rawJson = await loadScenario(5);
+    const jsonAst = parse(rawJson, { loc: true });
+    expect(
+      getMetaFromPath(jsonAst, '/foo/~1some~1path/value')
+    ).toMatchSnapshot();
+  });
+
+  it('can work with unescaped JSON pointers with ~0', async () => {
+    const rawJson = await loadScenario(5);
+    const jsonAst = parse(rawJson, { loc: true });
+    expect(
+      getMetaFromPath(jsonAst, '/foo/~0some~0path/value')
+    ).toMatchSnapshot();
   });
 });
