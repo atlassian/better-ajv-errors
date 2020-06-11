a487da1aa030137d2d19efd162697a5adc794e0a
Jorge Osés Grijalba <jorses@users.noreply.github.com>
2020-06-11 10:32:54 +0200

    Add better suggestions for enum and additional-prop errors

--- src/validation-errors/additional-prop.js
+++ src/validation-errors/additional-prop.js
@@ -1,4 +1,6 @@
 import BaseValidationError from './base';
+import leven from 'leven';
+import pointer from 'jsonpointer';
 
 export default class AdditionalPropValidationError extends BaseValidationError {
   constructor(...args) {
@@ -8,13 +10,47 @@ export default class AdditionalPropValidationError extends BaseValidationError {
 
   getError() {
     const { params, dataPath } = this.options;
-
-    return {
+    const bestMatch = this.findBestMatch();
+    //console.log(this.options)
+    const output = {
       ...this.getLocation(`${dataPath}/${params.additionalProperty}`),
       error: `${this.getDecoratedPath(dataPath)} Property ${
         params.additionalProperty
       } is not expected to be here`,
       path: dataPath,
+      suggestion: `Allowed values are: ${Object.keys(
+        this.schema.properties
+      ).join(', ')}`,
     };
+
+    if (bestMatch !== null) {
+      output.suggestion = `Did you mean '${bestMatch}'? ` + output.suggestion;
+    }
+    return output;
+  }
+
+  findBestMatch() {
+    const { params } = this.options;
+
+    const allowedValues = Object.keys(this.schema.properties);
+    const currentValue = params.additionalProperty;
+
+    if (!currentValue) {
+      return null;
+    }
+
+    const bestMatch = allowedValues
+      .map(value => ({
+        value,
+        weight: leven(value, currentValue.toString()),
+      }))
+      .sort((x, y) =>
+        x.weight > y.weight ? 1 : x.weight < y.weight ? -1 : 0
+      )[0];
+
+    return allowedValues.length === 1 ||
+      bestMatch.weight < bestMatch.value.length
+      ? bestMatch.value
+      : null;
   }
 }
--- src/validation-errors/default.js
+++ src/validation-errors/default.js
@@ -1,13 +1,66 @@
 import BaseValidationError from './base';
+import leven from 'leven';
+import pointer from 'jsonpointer';
 
 export default class DefaultValidationError extends BaseValidationError {
   getError() {
     const { keyword, message, dataPath } = this.options;
+    if (keyword != 'enum') {
+      return {
+        ...this.getLocation(),
+        error: `${this.getDecoratedPath(dataPath)}: ${keyword} ${message}`,
+        path: dataPath,
+      };
+    } else {
+      return this.getEnumError();
+    }
+  }
 
-    return {
+  getEnumError() {
+    const { message, dataPath, params } = this.options;
+    const bestMatch = this.findBestMatch();
+
+    const output = {
       ...this.getLocation(),
-      error: `${this.getDecoratedPath(dataPath)}: ${keyword} ${message}`,
+      error: `${this.getDecoratedPath(
+        dataPath
+      )} ${message}: ${params.allowedValues.join(', ')}`,
       path: dataPath,
+      suggestion: `Allowed values are: ${params.allowedValues.join(', ')}`,
     };
+
+    if (bestMatch !== null) {
+      output.suggestion = `Did you mean '${bestMatch}? ` + output.suggestion;
+    }
+
+    return output;
+  }
+
+  findBestMatch() {
+    const {
+      dataPath,
+      params: { allowedValues },
+    } = this.options;
+
+    const currentValue =
+      dataPath === '' ? this.data : pointer.get(this.data, dataPath);
+
+    if (!currentValue) {
+      return null;
+    }
+
+    const bestMatch = allowedValues
+      .map(value => ({
+        value,
+        weight: leven(value, currentValue.toString()),
+      }))
+      .sort((x, y) =>
+        x.weight > y.weight ? 1 : x.weight < y.weight ? -1 : 0
+      )[0];
+
+    return allowedValues.length === 1 ||
+      bestMatch.weight < bestMatch.value.length
+      ? bestMatch.value
+      : null;
   }
 }
