d4b2ffa8c202556c5dec223b3c51918f23fb4307
Jon Ursenbach <jon@ursenba.ch>
2021-10-11 12:09:53 -0700

    test: adding a test for the babel export

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,6 +1,7 @@
 import Ajv from 'ajv';
 import { getSchemaAndData } from '../test-helpers';
 import betterAjvErrors from '../';
+import betterAjvErrorsBabelExport from '../../lib';
 
 describe('Main', () => {
   it('should output error with reconstructed codeframe', async () => {
@@ -29,5 +30,18 @@ describe('Main', () => {
       json,
     });
     expect(res).toMatchSnapshot();
+  });
+
+  describe('Babel export', () => {
+    it('should function as normal', async () => {
+      const [schema, data] = await getSchemaAndData('default', __dirname);
+      const ajv = new Ajv({ jsonPointers: true });
+      const validate = ajv.compile(schema);
+      const valid = validate(data);
+      expect(valid).toBeFalsy();
+
+      const res = betterAjvErrorsBabelExport(schema, data, validate.errors);
+      expect(res).toMatchSnapshot();
+    });
   });
 });
