8776d875a4adc2c8f8158a1c809fbc128735e27e
Rifat Nabi <to.rifat@gmail.com>
2022-04-11 14:50:11 +1000

    Fix lint error & bump local dependencies

--- src/json/utils.js
+++ src/json/utils.js
@@ -3,10 +3,5 @@ export const getPointers = dataPath => {
   return dataPath
     .split('/')
     .slice(1)
-    .map((pointer) =>
-      pointer.split('~1')
-        .join('/')
-        .split('~0')
-        .join('~')
-    );
+    .map(pointer => pointer.split('~1').join('/').split('~0').join('~'));
 };
