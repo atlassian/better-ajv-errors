0ae81736de455967925a0d776765c72cf76d7993
David Sveningsson <ext@sidvind.com>
2024-09-08 17:52:18 +0200

    feat(deps): drop @babel/code-frame dependency

--- build.js
+++ build.js
@@ -23,7 +23,7 @@ esbuild
   .build({
     ...(isEsmBuild ? config.esm : config.cjs),
     entryPoints: ['src/index.js'],
-    external: ['@babel/code-frame', 'chalk'],
+    external: ['chalk'],
     bundle: true,
     sourcemap: true,
     logLevel: isCI ? 'silent' : 'info',
