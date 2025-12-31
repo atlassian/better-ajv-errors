a0ab45250af254748d1297e5673c351756ebcd39
Rifat Nabi <rnabi@atlassian.com>
2025-12-31 14:29:40 +1100

    Fix type export
    Closes #176

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,14 @@
+import path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      'better-ajv-errors': path.resolve(__dirname, './src/index.js'),
+    },
+  },
   test: {
-    include: ['src/**/__tests__/**/*.js'],
+    include: ['src/**/__tests__/**/*.js', 'typings.test-d.ts'],
     env: {
       CI: 'true',
     },
