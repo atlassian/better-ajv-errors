b12440cff610fc5683c86a196d72a1b4c461c659
Daniel Miller <dmiller3@atlassian.com>
2026-03-13 12:18:37 -0700

    [BUG] Allow parsing null in allowedValues
    Previously, when attempting to print errors for schemas that include
    nullable enums, the call of "leven" would error as null has no length.
    
    This commit changes the print function to only call for leven distance
    with nonnull values and also handle printing null values, as a simple
    join will only output ", ".
    
    BUG: [Issue-224](https://github.com/atlassian/better-ajv-errors/issues/224)

--- src/validation-errors/__tests__/enum.js
+++ src/validation-errors/__tests__/enum.js
@@ -4,117 +4,134 @@ import { getSchemaAndData } from '../../test-helpers';
 import EnumValidationError from '../enum';
 
 describe('Enum', () => {
-  describe('when value is an object', () => {
-    let schema, data, jsonRaw, jsonAst;
-    beforeAll(async () => {
-      [schema, data] = await getSchemaAndData('enum', __dirname);
-      jsonRaw = JSON.stringify(data, null, 2);
-      jsonAst = parse(jsonRaw);
-    });
+	describe.each([
+		['does not include', 'enum', []],
+		['includes', 'enum-with-nulls', [null]],
+	])('when enum $0 null', (_title, name, additionalAllowedValues) => {
+		describe('when value is an object', () => {
+			let schema, data, jsonRaw, jsonAst;
+			beforeAll(async () => {
+				[schema, data] = await getSchemaAndData(name, __dirname);
+				jsonRaw = JSON.stringify(data, null, 2);
+				jsonAst = parse(jsonRaw);
+			});
 
-    it('prints correctly for enum prop', () => {
-      const error = new EnumValidationError(
-        {
-          keyword: 'enum',
-          dataPath: '/id',
-          schemaPath: '#/enum',
-          params: { allowedValues: ['foo', 'bar'] },
-          message: `should be equal to one of the allowed values`,
-        },
-        { data, schema, jsonRaw, jsonAst }
-      );
+			it('prints correctly for enum prop', () => {
+				const error = new EnumValidationError(
+					{
+						keyword: 'enum',
+						dataPath: '/id',
+						schemaPath: '#/enum',
+						params: {
+							allowedValues: ['foo', 'bar'].concat(additionalAllowedValues),
+						},
+						message: `should be equal to one of the allowed values`,
+					},
+					{ data, schema, jsonRaw, jsonAst }
+				);
 
-      expect(error.print()).toMatchSnapshot();
-    });
+				expect(error.print()).toMatchSnapshot();
+			});
 
-    it('prints correctly for no levenshtein match', () => {
-      const error = new EnumValidationError(
-        {
-          keyword: 'enum',
-          dataPath: '/id',
-          schemaPath: '#/enum',
-          params: { allowedValues: ['one', 'two'] },
-          message: `should be equal to one of the allowed values`,
-        },
-        { data, schema, jsonRaw, jsonAst }
-      );
+			it('prints correctly for no levenshtein match', () => {
+				const allowedValues = ['one', 'two'].concat(additionalAllowedValues);
+				const error = new EnumValidationError(
+					{
+						keyword: 'enum',
+						dataPath: '/id',
+						schemaPath: '#/enum',
+						params: {
+							allowedValues,
+						},
+						message: `should be equal to one of the allowed values`,
+					},
+					{ data, schema, jsonRaw, jsonAst }
+				);
 
-      expect(error.print()).toMatchSnapshot();
-    });
+				expect(error.print()).toMatchSnapshot();
+			});
 
-    it('prints correctly for empty value', () => {
-      const error = new EnumValidationError(
-        {
-          keyword: 'enum',
-          dataPath: '/id',
-          schemaPath: '#/enum',
-          params: { allowedValues: ['foo', 'bar'] },
-          message: `should be equal to one of the allowed values`,
-        },
-        { data, schema, jsonRaw, jsonAst }
-      );
+			it('prints correctly for empty value', () => {
+				const error = new EnumValidationError(
+					{
+						keyword: 'enum',
+						dataPath: '/id',
+						schemaPath: '#/enum',
+						params: {
+							allowedValues: ['foo', 'bar'].concat(additionalAllowedValues),
+						},
+						message: `should be equal to one of the allowed values`,
+					},
+					{ data, schema, jsonRaw, jsonAst }
+				);
 
-      expect(error.print(schema, { id: '' })).toMatchSnapshot();
-    });
-  });
+				expect(error.print(schema, { id: '' })).toMatchSnapshot();
+			});
+		});
+	});
 
-  describe('when value is a primitive', () => {
-    let schema, data, jsonRaw, jsonAst;
-    beforeAll(async () => {
-      [schema, data] = await getSchemaAndData('enum-string', __dirname);
-      jsonRaw = JSON.stringify(data, null, 2);
-      jsonAst = parse(jsonRaw);
-    });
+	describe.each([
+		['does not include', 'enum-string', []],
+		['includes', 'enum-string-with-nulls', [null]],
+	])('when enum $0 null', (_title, name, additionalAllowedValues) => {
+		describe('when value is a primitive', () => {
+			let schema, data, jsonRaw, jsonAst;
+			beforeAll(async () => {
+				[schema, data] = await getSchemaAndData(name, __dirname);
+				jsonRaw = JSON.stringify(data, null, 2);
+				jsonAst = parse(jsonRaw);
+			});
 
-    it('prints correctly for enum prop', () => {
-      const error = new EnumValidationError(
-        {
-          keyword: 'enum',
-          dataPath: '',
-          schemaPath: '#/enum',
-          params: {
-            allowedValues: ['foo', 'bar'],
-          },
-          message: 'should be equal to one of the allowed values',
-        },
-        { data, schema, jsonRaw, jsonAst }
-      );
+			it('prints correctly for enum prop', () => {
+				const error = new EnumValidationError(
+					{
+						keyword: 'enum',
+						dataPath: '',
+						schemaPath: '#/enum',
+						params: {
+							allowedValues: ['foo', 'bar'].concat(additionalAllowedValues),
+						},
+						message: 'should be equal to one of the allowed values',
+					},
+					{ data, schema, jsonRaw, jsonAst }
+				);
 
-      expect(error.print()).toMatchSnapshot();
-    });
+				expect(error.print()).toMatchSnapshot();
+			});
 
-    it('prints correctly for no levenshtein match', () => {
-      const error = new EnumValidationError(
-        {
-          keyword: 'enum',
-          dataPath: '',
-          schemaPath: '#/enum',
-          params: {
-            allowedValues: ['one', 'two'],
-          },
-          message: 'should be equal to one of the allowed values',
-        },
-        { data, schema, jsonRaw, jsonAst }
-      );
+			it('prints correctly for no levenshtein match', () => {
+				const error = new EnumValidationError(
+					{
+						keyword: 'enum',
+						dataPath: '',
+						schemaPath: '#/enum',
+						params: {
+							allowedValues: ['one', 'two'].concat(additionalAllowedValues),
+						},
+						message: 'should be equal to one of the allowed values',
+					},
+					{ data, schema, jsonRaw, jsonAst }
+				);
 
-      expect(error.print()).toMatchSnapshot();
-    });
+				expect(error.print()).toMatchSnapshot();
+			});
 
-    it('prints correctly for empty value', () => {
-      const error = new EnumValidationError(
-        {
-          keyword: 'enum',
-          dataPath: '',
-          schemaPath: '#/enum',
-          params: {
-            allowedValues: ['foo', 'bar'],
-          },
-          message: 'should be equal to one of the allowed values',
-        },
-        { data, schema, jsonRaw, jsonAst }
-      );
+			it('prints correctly for empty value', () => {
+				const error = new EnumValidationError(
+					{
+						keyword: 'enum',
+						dataPath: '',
+						schemaPath: '#/enum',
+						params: {
+							allowedValues: ['foo', 'bar'].concat(additionalAllowedValues),
+						},
+						message: 'should be equal to one of the allowed values',
+					},
+					{ data, schema, jsonRaw, jsonAst }
+				);
 
-      expect(error.print(schema, '')).toMatchSnapshot();
-    });
-  });
+				expect(error.print(schema, '')).toMatchSnapshot();
+			});
+		});
+	});
 });
